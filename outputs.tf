e9eebc1e9845ec3221e234d937014b35c314dd6d
Guy Feldman <guy.feldman@cymatic.io>
2020-04-11 20:27:43 -0400

    removing issuer arn

--- outputs.tf
+++ outputs.tf
@@ -57,10 +57,6 @@ output "cluster_oidc_issuer_url" {
   value       = flatten(concat(aws_eks_cluster.this[*].identity[*].oidc.0.issuer, [""]))[0]
 }
 
-output "cluster_oidc_issuer_arn" {
-  description = "The ARN on the EKS cluster OIDC Issuer"
-  value       = flatten(concat(aws_eks_cluster.this[*].identity.arn, [""]))[0]
-}
 
 output "cloudwatch_log_group_name" {
   description = "Name of cloudwatch log group created"
